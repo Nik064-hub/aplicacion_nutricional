b93283d029b736af1f4fe4a2ceaa74d7f8c1617d
Nik064-hub <saguirre@estud.usfq.edu.ec>
2025-11-17 14:17:43 -0500

    Se crea hash_utils.dart

---
+++ lib/core/models/hash_utils.dart
@@ -0,0 +1,12 @@
+// lib/core/utils/hash_utils.dart
+
+import 'dart:convert';
+import 'package:crypto/crypto.dart';
+
+class HashUtils {
+  static String hashPassword(String password) {
+    final bytes = utf8.encode(password);
+    final digest = sha256.convert(bytes);
+    return digest.toString();
+  }
+}
